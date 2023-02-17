fd7dedd0dc5052bf67da00024be0dff33307796d
farya <ftjamal2017@gmail.com>
2023-02-18 04:47:21 +0500

    DONE

---
+++ myapp/src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+
+function App() {
+
+  let count = 0;
+  let saveEl = document.getElementById("save-el")
+  let countEl = document.getElementById("count-el")
+
+  function increment() {
+    count += 1 //   count = count + 1;
+    countEl.innerHTML = count
+    // OR  document.getElementById("count-el").innerHTML = count
+  }
+
+  function save() {
+    let countStr = count + " - ";
+    // saveEl = saveEl.textContenttStr + countStr
+    saveEl.innerHTML += countStr
+    countEl.textContent = 0
+    count = 0
+  }
+
+  console.log("Let's count people on the subway!")
+
+
+
+
+  return (
+    <div className="App">
+      <h1>People entered:</h1>
+      <h2 id="count-el">0</h2>
+      <button id="increment-btn" onClick={() => increment()}>INCREMENT</button>
+      <button id="save-btn" onClick={() => save()}>SAVE</button>
+      <p id="save-el">Previous entries: </p>
+
+    </div>
+
+  );
+}
+
+export default App;
